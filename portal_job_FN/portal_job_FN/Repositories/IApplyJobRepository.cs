116deef5ba04faa397e07fe347cc7957e62ed097
ChauThLong <longchau5823@gmail.com>
2024-10-17 02:58:31 +0700

    push code_Long

---
+++ portal_job_FN/portal_job_FN/Repositories/IApplyJobRepository.cs
@@ -0,0 +1,15 @@
+﻿using portal_job_FN.Models;
+
+namespace portal_job_FN.Repositories
+{
+    public interface IApplyJobRepository
+    {
+        Task<IEnumerable<ApplyJob>> GetAllAsync();
+        Task<ApplyJob> GetByIdAsync(int id);
+        Task<IEnumerable<ApplyJob>> GetAllApplyByUserIdAsync(string id);
+        Task<IEnumerable<ApplyJob>> GetAllApplyByCompanyIdAsync(string id);
+        Task AddAsync(ApplyJob apply_Job);
+        Task UpdateAsync(ApplyJob apply_Job);
+        Task DeleteAsync(int id);
+    }
+}
---
+++ portal_job_FN/portal_job_FN/Repositories/IPostJobRepository.cs
@@ -0,0 +1,15 @@
+﻿using portal_job_FN.Models;
+
+namespace portal_job_FN.Repositories
+{
+    public interface IPostJobRepository
+    {
+        Task<IEnumerable<PostJob>> GetAllAsync();
+        Task<IEnumerable<PostJob>> GetAllByCompanyIdAsync(string id);
+        Task<PostJob> GetByIdAsync(int id);
+        Task AddAsync(PostJob post_Job);
+        Task UpdateAsync(PostJob post_Job);
+        Task DeleteAsync(int id);
+
+    }
+}
